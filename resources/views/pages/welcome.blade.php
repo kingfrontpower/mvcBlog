537d701ad88707c969ffbbdcddc3c277ee46cddd
FRONTPOWER-HP\Frontpower <kingfrontpower@gamil.com>
2017-12-31 17:30:38 +0800

    modify about_welcome_contact_page_and_remove_publicUrl

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -19,6 +19,7 @@
 <![endif]-->
     </head>
     <body>
+        <!--       bootstrap basic nav-->
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -29,37 +30,28 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Brand</a>
+                    <a class="navbar-brand" href="#">Laravel Blog</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">Link</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
+                        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li><a href="about">About</a></li>
+                        <li><a href="contact">Contact</a></li>
                     </ul>
-                    <form class="navbar-form navbar-left">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
+                    <!--
+<form class="navbar-form navbar-left">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Search">
+</div>
+<button type="submit" class="btn btn-default">Submit</button>
+</form>
+-->
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Link</a></li>
+                        <!--                        <li><a href="#">Link</a></li>-->
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Action</a></li>
                                 <li><a href="#">Another action</a></li>
@@ -72,6 +64,58 @@
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
         </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jumbotron">
+                        <h1>Welcome to My Blog!</h1>
+                        <p class='lead'>Thank you so much for visiting. This is my test website built with Laravel. Please read my popular post!</p>
+                        <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
+                    </div>
+
+                </div>
+            </div><!-- end of header.row -->
+            <div class="row">
+                <div class="col-md-8" >
+                    <div class="post">
+                        <h3>Post Title</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, cum quas reprehenderit optio delectus voluptatibus est error quam aliquam voluptate, voluptates quo dolor iste nesciunt odio, tempora fuga minus sequi!</p>
+                        <a href="#" class='btn btn-primary'>Read More</a>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="post">
+                        <h3>Post Title</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, cum quas reprehenderit optio delectus voluptatibus est error quam aliquam voluptate, voluptates quo dolor iste nesciunt odio, tempora fuga minus sequi!</p>
+                        <a href="#" class='btn btn-primary'>Read More</a>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="post">
+                        <h3>Post Title</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, cum quas reprehenderit optio delectus voluptatibus est error quam aliquam voluptate, voluptates quo dolor iste nesciunt odio, tempora fuga minus sequi!</p>
+                        <a href="#" class='btn btn-primary'>Read More</a>
+                    </div>
+                    
+                    <hr />
+                    
+                    <div class="post">
+                        <h3>Post Title</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, cum quas reprehenderit optio delectus voluptatibus est error quam aliquam voluptate, voluptates quo dolor iste nesciunt odio, tempora fuga minus sequi!</p>
+                        <a href="#" class='btn btn-primary'>Read More</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1"  >
+                    Sidebar
+
+                </div>
+
+            </div>
+
+        </div><!-- end of container -->
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
