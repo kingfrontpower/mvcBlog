5b46ad6ea517aea1b5c97f4ec08307b112f4f6ca
FRONTPOWER-HP\Frontpower <kingfrontpower@gamil.com>
2018-01-01 11:58:36 +0800

    yield('content')

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,102 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Laravel Blog - Contact Us</title>
-
-        <!-- Bootstrap -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-    </head>
-    <body>
-        <!--       bootstrap basic nav-->
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Laravel Blog</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li ><a href="index.php">Home </a></li>
-                        <li ><a href="about">About</a></li>
-                        <li class="active"><a href="contact">Contact</a></li>
-                    </ul>
-                    <!--
-<form class="navbar-form navbar-left">
-<div class="form-group">
-<input type="text" class="form-control" placeholder="Search">
-</div>
-<button type="submit" class="btn btn-default">Submit</button>
-</form>
--->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!--                        <li><a href="#">Link</a></li>-->
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Contact Us</h1>
-                    <hr />
-                    <form action="" >
-                        <div class="form-group">
-                            <label name="email">Email:</label>
-                            <input type="email" name='email' id="email" class='form-control'>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label name="subject">Subject:</label>
-                            <input type="subject" name='subject' id="subject" class='form-control'>
-                        </div>
-                        
-                        <div class="form-group">
-                            <label name="message">message:</label>
-                            <textarea type="text" name='message' id="message" class='form-control' placeholder='Type your message here...'></textarea>
-                        </div>
-                        
-                        <input type="submit" value='Send Message' class="btn btn-success">
-
-
-
-                    </form>
-                </div>
+@extends('main')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1>Contact Us</h1>
+        <hr />
+        <form action="" >
+            <div class="form-group">
+                <label name="email">Email:</label>
+                <input type="email" name='email' id="email" class='form-control'>
             </div>
-        </div>
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </body>
-</html>
+
+            <div class="form-group">
+                <label name="subject">Subject:</label>
+                <input type="subject" name='subject' id="subject" class='form-control'>
+            </div>
+
+            <div class="form-group">
+                <label name="message">message:</label>
+                <textarea type="text" name='message' id="message" class='form-control' placeholder='Type your message here...'></textarea>
+            </div>
+
+            <input type="submit" value='Send Message' class="btn btn-success">
+
+
+
+        </form>
+    </div>
+</div>
+@endsection
