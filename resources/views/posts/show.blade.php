8377213d5b27a7d6869cd2bdceeba03b4449773f
FRONTPOWER-HP\Frontpower <kingfrontpower@gamil.com>
2018-01-06 10:39:22 +0800

    Part 15 - Working with Dates from Database

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,36 @@
 
 @section('content')
 
-<h1>{{ $post->title }}</h1>
-<p>{{ $post->body }}</p>
+<div class="row">
+    <div class="col-md-8">
+        <h1>{{ $post->title }}</h1>
+        <p>{{ $post->body }}</p>
+    </div>
+    <div class="col-md-4">
+        <div class="well">
+            <dl class='dl-horizontal'>
+                <dt>Create At:</dt>
+                <dd>{{ date('M j, Y h:ia',strtotime($post->created_at)) }}</dd>
+            </dl>
+
+            <dl class='dl-horizontal'>
+                <dt>Last Updated:</dt>
+                <dd>{{ date('M j, Y h:ia',strtotime($post->updated_at)) }}</dd>
+            </dl>
+            <hr />
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Html::linkRoute('posts.edit','編輯',array($post->id),array('class'=>'btn btn-primary btn-block')) !!}
+
+                </div>
+                <div class="col-sm-6">
+                    {!! Html::linkRoute('posts.destroy','刪除',array($post->id),array('class'=>'btn btn-danger btn-block')) !!}
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
